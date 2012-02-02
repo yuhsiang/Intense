4eac9bc1e2aea8ee4aa69295ed8ab551a169f2e4
YuHsiang Lin <yuhsianglin@YuHsiangs-MacBook-Pro.local>
2012-02-02 12:21:42 -0500

    need rotation for dart

--- DartBalloon/Assets/scripts/DartMovement.cs
+++ DartBalloon/Assets/scripts/DartMovement.cs
@@ -6,27 +6,43 @@ public class DartMovement : MonoBehaviour {
 	public Transform PopEffect;
 	private Vector3 mouseDownPosition;
 	private Vector3 mouseUpPosition;
-
+	private bool hasAppliedForce = false;
+	private Vector3 direction;
+	private float force ;
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButton(0)){
-			//Vector3 position = (new Vector3(Input.mousePosition.x, Input.GetAxis("Mouse Y"), dart.transform.position.z));
-			
-			mouseDownPosition = new Vector3(Input.GetAxis("Mouse X") 
-			                                ,Input.GetAxis("Mouse Y"), transform.position.z);
-			Debug.LogWarning(mouseDownPosition.x + ", "+mouseDownPosition.y);
-			
-   			//this.transform.Translate(Vector3.up * Input.GetAxis("Mouse Y")+Vector3.right*Input.GetAxis("Mouse X"),  Space.World);
-		} else if (Input.GetMouseButtonUp(0)){
-			mouseUpPosition = new Vector3(Input.GetAxis("Mouse X") 
-			                                ,Input.GetAxis("Mouse Y"), transform.position.z);
-			Vector3 force = new Vector3(mouseDownPosition.x-mouseUpPosition.x, mouseDownPosition.y-mouseUpPosition.y, mouseDownPosition.z);
-			rigidbody.AddForce(force*10);
-			Debug.LogWarning("mouseDown");
-			
+	void FixedUpdate(){
+		if (hasAppliedForce){
+					this.transform.Translate(direction * force/130,  Space.World);
 		}
 	}
+		
+	void Update(){
+		if (Input.GetMouseButtonDown(0) &&!hasAppliedForce){
+			//Vector3 position = (new Vector3(Input.mousePosition.x, Input.GetAxis("Mouse Y"), dart.transform.position.z));
+			
+			mouseDownPosition = new Vector3(Input.mousePosition.x
+			                                ,Input.mousePosition.y, transform.position.z);
+			
+			
+			
+   			//this.transform.Translate(Vector3.up * Input.GetAxis("Mouse Y")+Vector3.right*Input.GetAxis("Mouse X"),  Space.World);
+		} else if (Input.GetMouseButtonUp(0)&&!hasAppliedForce){
+	
+			mouseUpPosition = new Vector3(Input.mousePosition.x
+			                                ,Input.mousePosition.y, transform.position.z);
+			direction = Vector3.up*(mouseUpPosition.y-mouseDownPosition.y)+ Vector3.right*(mouseUpPosition.x-mouseDownPosition.x);
+			force = direction.magnitude;
+			
+			direction.Normalize();
+			
+			this.transform.Translate(direction * force/130,  Space.World);
+			
+			this.transform.Rotate(direction.x, direction.y, 0);
+			hasAppliedForce = true;
+			
+		}	
+	}
+	
 	
 	void OnCollisionEnter(Collision collision ) {
 		if (collision.gameObject.name.CompareTo("Sphere")==0){
@@ -40,7 +56,7 @@ public class DartMovement : MonoBehaviour {
 			
 			
 		} else {
-			Debug.LogWarning("no");
+			//Debug.LogWarning("no");
 		}
 		
 		
