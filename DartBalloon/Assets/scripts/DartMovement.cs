79ed47fc9d33c8535c8b384031814deca4e14be1
YuHsiang Lin <yuhsianglin@YuHsiangs-MacBook-Pro.local>
2012-02-03 01:37:39 -0500

    direction working

--- DartBalloon/Assets/scripts/DartMovement.cs
+++ DartBalloon/Assets/scripts/DartMovement.cs
@@ -9,35 +9,29 @@ public class DartMovement : MonoBehaviour {
 	private bool hasAppliedForce = false;
 	private Vector3 direction;
 	private float force ;
-	
+	private const int SPEED_FACTOR = 200;
 	void FixedUpdate(){
 		if (hasAppliedForce){
-					this.transform.Translate(direction * force/130,  Space.World);
+			this.transform.Translate(direction * force/SPEED_FACTOR,  Space.World);
 		}
 	}
 		
 	void Update(){
 		if (Input.GetMouseButtonDown(0) &&!hasAppliedForce){
-			//Vector3 position = (new Vector3(Input.mousePosition.x, Input.GetAxis("Mouse Y"), dart.transform.position.z));
-			
+		
 			mouseDownPosition = new Vector3(Input.mousePosition.x
 			                                ,Input.mousePosition.y, transform.position.z);
 			
-			
-			
-   			//this.transform.Translate(Vector3.up * Input.GetAxis("Mouse Y")+Vector3.right*Input.GetAxis("Mouse X"),  Space.World);
+   		
 		} else if (Input.GetMouseButtonUp(0)&&!hasAppliedForce){
-	
 			mouseUpPosition = new Vector3(Input.mousePosition.x
 			                                ,Input.mousePosition.y, transform.position.z);
 			direction = Vector3.up*(mouseUpPosition.y-mouseDownPosition.y)+ Vector3.right*(mouseUpPosition.x-mouseDownPosition.x);
-			force = direction.magnitude;
-			
+			force = direction.magnitude;	
+			transform.LookAt(new Vector3(-direction.x
+			                                ,-direction.y, transform.position.z));
 			direction.Normalize();
-			
-			this.transform.Translate(direction * force/130,  Space.World);
-			
-			this.transform.Rotate(direction.x, direction.y, 0);
+			this.transform.Translate(direction * force/SPEED_FACTOR,  Space.World);	
 			hasAppliedForce = true;
 			
 		}	
@@ -51,9 +45,6 @@ public class DartMovement : MonoBehaviour {
 			// Quaternion.identity -> The identity rotation (Read Only). This quaternion corresponds to "no rotation": the object
 			//
 			Instantiate(PopEffect, collision.transform.position,  Quaternion.identity);
-			
-			
-			
 			
 		} else {
 			//Debug.LogWarning("no");
---
+++ DartBalloon/Assets/scripts/animation.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class animation : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		animation.Blend("idle");
+	}
+}
